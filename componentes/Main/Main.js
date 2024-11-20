b2c2d03c1a4b4ffa6c6c95c15df26653c88fade1
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-11-20 17:48:21 +0000

    update

--- componentes/Main/Main.js
+++ componentes/Main/Main.js
@@ -1,14 +1,23 @@
 import "./Main.css";
 
-const template = () => {`
+const template = () => `
 <h2 id="message"></h2>
-<ul id="resultS"></ul>
+<ul id="results"></ul>
     
-`
+`;
+
+//Crearemos una función adicional
+const listeners = () => {
+
+}
+//Dicha función la ejecutaremos a continuación dentro de nuestro Main
 
 const Main = () => {
 document.querySelector("main").innerHTML = template();
 
 }
 
-}
+
+
+
+export default Main;
--- pinterest/main.js
+++ pinterest/main.js
@@ -1,12 +1,38 @@
 import './style.css'
 import Header from '../componentes/Header/Header';
+import Main from '../componentes/Main/Main';
 import Footer from '../componentes/footer/footer';
+
+
+const  CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
  
 const buildWebsite = () => {
 Header()
-
+Main ()
 Footer()
-
+getPhotos("space")
 };
 
+const getPhotos = async (keyword) => {
+const data =  await fetch(`https://api.unsplash.com/search/photos?page=1&query=${keyword}&per_page=20&client_id=${CLIENT_ID}`   
+);
+const results = await data.json();
+const photos = results.results;
+console.log(photos);
+printPhotos(photos);
+};
+
+ const printPhotos = (photos) =>{
+const container = document.querySelector("#results");
+const message = document.querySelector("#message");
+
+for (const photo of photos) {//Creamos un bucle que recorra cada una de las fotos
+const li = document.createElement("li"); //creamos un li por cada foto
+li.innerHTML = `
+<img src=${photo.urls.regular} alt = "${photo.alt_description}" />
+`
+
+container.appendChild(li);
+ } 
+ }
 buildWebsite();
